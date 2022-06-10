f20d7fe29b86de0cee7990d4f1300ee150279ba3
AiswaryaVilsan97 <99252788+AiswaryaVilsan97@users.noreply.github.com>
2022-06-10 07:23:26 +0530

    Update Access.java

--- json/src/main/java/Access.java
+++ json/src/main/java/Access.java
@@ -21,42 +21,24 @@ public class Access extends HttpServlet{
 		res.setCharacterEncoding("UTF-8");
 		String empl_name =req.getParameter("username");
 		String role= req.getParameter("password");
-		
 		PrintWriter out= res.getWriter();
 		Employee emp= new Employee();
 		Connection c=null;
-	    PreparedStatement ps=null;
-	    Statement st=null;
-	    ResultSet r=null;		
+	        PreparedStatement ps=null;
+	        Statement st=null;
+	        ResultSet r=null;		
 		 try {
 			 if(emp.validate(empl_name,role)) {
 				Class.forName("com.mysql.jdbc.Driver");
-			 c=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "Current-Root-Password");
-			 if(role.equals("admin")) {
-				System.out.println("CONNECTION SUCCESSFUL");								
-		 		st= c.createStatement();			
-				r = st.executeQuery("SELECT * FROM employee.employee");
-				while (r.next()) {
-		             		JSONArray jsonr= new JSONArray();
-			    		 JSONObject jo= new JSONObject();
+			        c=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "Current-Root-Password");
+			        if(role.equals("admin")) {
+					System.out.println("CONNECTION SUCCESSFUL");								
+		 			st= c.createStatement();			
+					r = st.executeQuery("SELECT * FROM employee.employee");
+					while (r.next()) {
+		             			JSONArray jsonr= new JSONArray();
+			    			JSONObject jo= new JSONObject();
 					
-			                jo.put("empl_id", r.getLong("empl_id"));
-					jo.put("id", r.getLong("id"));
-					jo.put("empl_name", r.getString("empl_name"));
-					jo.put("phone_number", r.getLong("phone_number"));
-					jo.put("place", r.getString("place"));
-					jo.put("role", r.getString("role")); 
-				 
-			                jsonr.add(jo);
-			                out.println(jsonr);		
-					}}
-				 else {
-					 ps= c.prepareStatement("SELECT * FROM organisation.employeedetails  where empl_name=?");   
-		        		 ps.setString(3,empl_name);
-		        		 ResultSet rs = ps.executeQuery();
-		        		 while (rs.next()) {
-		         	 		JSONArray jsonr= new JSONArray();
-			     	 		JSONObject jo= new JSONObject();
 			                	jo.put("empl_id", r.getLong("empl_id"));
 						jo.put("id", r.getLong("id"));
 						jo.put("empl_name", r.getString("empl_name"));
@@ -64,9 +46,26 @@ public class Access extends HttpServlet{
 						jo.put("place", r.getString("place"));
 						jo.put("role", r.getString("role")); 
 				 
-			               		 jsonr.add(jo);
-			               		 out.println(jsonr);
+			                	jsonr.add(jo);
+			                	out.println(jsonr);		
+						}}
+					 else {
+					 	ps= c.prepareStatement("SELECT * FROM organisation.employeedetails  where empl_name=?");   
+		        		 	ps.setString(3,empl_name);
+		        		 	ResultSet rs = ps.executeQuery();
+		        		 	while (rs.next()) {
+		         	 			JSONArray jsonr= new JSONArray();
+			     	 			JSONObject jo= new JSONObject();
+			                		jo.put("empl_id", r.getLong("empl_id"));
+							jo.put("id", r.getLong("id"));
+							jo.put("empl_name", r.getString("empl_name"));
+							jo.put("phone_number", r.getLong("phone_number"));
+							jo.put("place", r.getString("place"));
+							jo.put("role", r.getString("role")); 
+				 
+			               		 	jsonr.add(jo);
+			               		 	out.println(jsonr);
 		         				}}}}				   		 
-						 catch(Exception e) {			
-						e.printStackTrace();
+						 	catch(Exception e) {			
+							e.printStackTrace();
 							}}}
