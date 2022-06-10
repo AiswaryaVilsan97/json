10bffc93a31a9fabc7e809abb3afe6001b47d8ce
AiswaryaVilsan97 <99252788+AiswaryaVilsan97@users.noreply.github.com>
2022-06-10 07:20:53 +0530

    Update Access.java

--- json/src/main/java/Access.java
+++ json/src/main/java/Access.java
@@ -9,8 +9,6 @@ import java.sql.Statement;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,7 +16,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class Access extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException,ServletException {
 		res.setContentType("application/json");
 		res.setCharacterEncoding("UTF-8");
@@ -30,52 +27,46 @@ public class Access extends HttpServlet{
 		Connection c=null;
 	    PreparedStatement ps=null;
 	    Statement st=null;
-	    ResultSet r=null;
-		
+	    ResultSet r=null;		
 		 try {
 			 if(emp.validate(empl_name,role)) {
 				Class.forName("com.mysql.jdbc.Driver");
 			 c=DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "root", "Current-Root-Password");
-
-			if(role.equals("admin")) {
-				System.out.println("CONNECTION SUCCESSFUL");
-
-								
-		 st= c.createStatement();
-			
-			r = st.executeQuery("SELECT * FROM employee.employee");
-			 while (r.next()) {
-		         JSONArray jsonr= new JSONArray();
-			     JSONObject jo= new JSONObject();
-			     jo.put("empl_id", r.getLong("empl_id"));
+			 if(role.equals("admin")) {
+				System.out.println("CONNECTION SUCCESSFUL");								
+		 		st= c.createStatement();			
+				r = st.executeQuery("SELECT * FROM employee.employee");
+				while (r.next()) {
+		             		JSONArray jsonr= new JSONArray();
+			    		 JSONObject jo= new JSONObject();
+					
+			                jo.put("empl_id", r.getLong("empl_id"));
 					jo.put("id", r.getLong("id"));
 					jo.put("empl_name", r.getString("empl_name"));
 					jo.put("phone_number", r.getLong("phone_number"));
 					jo.put("place", r.getString("place"));
-					jo.put("role", r.getString("role"));
-			     jsonr.add(jo);
-			     out.println(jsonr);
-		
-			}}else {
-				 ps= c.prepareStatement("SELECT * FROM organisation.employeedetails  where empl_name=?");   
-		         ps.setString(3,empl_name);
-		         ResultSet rs = ps.executeQuery();
-		         while (rs.next()) {
-		         JSONArray jsonr= new JSONArray();
-			     JSONObject jo= new JSONObject();
-			     jo.put("empl_id", r.getLong("empl_id"));
-					jo.put("id", r.getLong("id"));
-					jo.put("empl_name", r.getString("empl_name"));
-					jo.put("phone_number", r.getLong("phone_number"));
-					jo.put("place", r.getString("place"));
-					jo.put("role", r.getString("role"));
-			        jsonr.add(jo);
-			     out.println(jsonr);
-		         }
-			}		
-		 }
-		 }
-		 catch(Exception e) {
-			
-			e.printStackTrace();
-		}}}
+					jo.put("role", r.getString("role")); 
+				 
+			                jsonr.add(jo);
+			                out.println(jsonr);		
+					}}
+				 else {
+					 ps= c.prepareStatement("SELECT * FROM organisation.employeedetails  where empl_name=?");   
+		        		 ps.setString(3,empl_name);
+		        		 ResultSet rs = ps.executeQuery();
+		        		 while (rs.next()) {
+		         	 		JSONArray jsonr= new JSONArray();
+			     	 		JSONObject jo= new JSONObject();
+			                	jo.put("empl_id", r.getLong("empl_id"));
+						jo.put("id", r.getLong("id"));
+						jo.put("empl_name", r.getString("empl_name"));
+						jo.put("phone_number", r.getLong("phone_number"));
+						jo.put("place", r.getString("place"));
+						jo.put("role", r.getString("role")); 
+				 
+			               		 jsonr.add(jo);
+			               		 out.println(jsonr);
+		         				}}}}				   		 
+						 catch(Exception e) {			
+						e.printStackTrace();
+							}}}
